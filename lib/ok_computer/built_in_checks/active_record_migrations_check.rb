bd4a904c887a85ca77ef9a954250b701a91e8768
Dewey Garwood <dgarwood@users.noreply.github.com>
2025-02-11 11:13:02 -0700

    update migration check to use public active_record methods and errors (#14)

--- lib/ok_computer/built_in_checks/active_record_migrations_check.rb
+++ lib/ok_computer/built_in_checks/active_record_migrations_check.rb
@@ -2,36 +2,37 @@ module OkComputer
   class ActiveRecordMigrationsCheck < Check
     # Public: Check if migrations are pending or not
     def check
-      return unsupported unless supported?
+      return mark_message(page_load_message) if check_on_page_load?
 
-      if needs_migration?
-        mark_failure
-        mark_message "Pending migrations"
-      else
-        mark_message "NO pending migrations"
-      end
-    end
+      error_if_pending_migrations(Rails.version)
 
-    def needs_migration?
-      if ActiveRecord::Migrator.respond_to?(:needs_migration?) # Rails <= 5.1
-        ActiveRecord::Migrator.needs_migration?
-      else # Rails >= 5.2
-        ActiveRecord::Base.connection.migration_context.needs_migration?
-      end
-    end
-
-    def supported?
-      ActiveRecord::Migrator.respond_to?(:needs_migration?) ||
-        (ActiveRecord::Base.connection.respond_to?(:migration_context) &&
-         ActiveRecord::Base.connection.migration_context.respond_to?(:needs_migration?))
+      mark_message "NO pending migrations"
+    rescue ActiveRecord::PendingMigrationError
+      mark_failure
+      mark_message "Pending migrations"
     end
 
     private
 
-    # Private: Fail the check if ActiveRecord cannot check migration status
-    def unsupported
-      mark_failure
-      mark_message "This version of ActiveRecord does not support checking whether migrations are pending"
+    # this check is only valid if config.active_record.migration_error is set to false rather than :page_load
+    # :page_load adds a Middleware check that throws an error before the call reaches ok_computer
+    # we only run this check when :page_load is set if there are no pending migrations
+    # :page_load is the default in development, otherwise this is false
+    # see https://github.com/rails/rails/blob/d39db5d1891f7509cde2efc425c9d69bbb77e670/activerecord/lib/active_record/railtie.rb#L103
+    def check_on_page_load?
+      Rails.configuration.active_record.migration_error
+    end
+
+    def error_if_pending_migrations(version_string)
+      if Gem::Version.new(version_string) >= Gem::Version.new("7.1")
+        ActiveRecord::Migration.check_all_pending!
+      else
+        ActiveRecord::Migration.check_pending!
+      end
+    end
+
+    def page_load_message
+      "NOTE: pending migrations are checked on page_load"
     end
   end
 end
--- spec/ok_computer/built_in_checks/active_record_migrations_check_spec.rb
+++ spec/ok_computer/built_in_checks/active_record_migrations_check_spec.rb
@@ -2,49 +2,51 @@ require "rails_helper"
 
 module OkComputer
   describe ActiveRecordMigrationsCheck do
+    let(:rails_pending_migration_method) do
+      if Gem::Version.new(Rails.version) >= Gem::Version.new("7.1")
+        # rails now supports multiple databases and checks
+        # that any of them have pending migrations
+        :check_all_pending!
+      else
+        :check_pending!
+      end
+    end
+
     it "is a subclass of Check" do
       expect(subject).to be_a Check
     end
 
-    current_rails_version = Gem::Version.new(ActiveRecord::VERSION::STRING)
-
-    # 4.0 <= Rails < 7.2
-    if current_rails_version > Gem::Version.new("3.99.99") && current_rails_version < Gem::Version.new("7.2")
-      context "when activerecord supports needs_migration?" do
-        context "#supported?" do
-          it { expect(subject.supported?).to be_truthy }
-        end
-
-        context "#check" do
-          context "with no pending migrations" do
-            before do
-              expect(subject).to receive(:needs_migration?).and_return(false)
-            end
-
-            it { is_expected.to be_successful_check }
-            it { is_expected.to have_message "NO pending migrations" }
-          end
-
-          context "with pending migrations" do
-            before do
-              expect(subject).to receive(:needs_migration?).and_return(true)
-            end
-
-            it { is_expected.not_to be_successful_check }
-            it { is_expected.to have_message "Pending migrations" }
-          end
-        end
+    context "#check" do
+      before do
+        allow(Rails.configuration.active_record).to receive(:migration_error).and_return(nil)
       end
-    else # Rails <= 3.2
-      context "when on older versions of ActiveRecord" do
-        context "#supported?" do
-          it { expect(subject.supported?).to be_falsey }
+
+      context "with no pending migrations" do
+        before do
+          expect(ActiveRecord::Migration).to receive(rails_pending_migration_method).and_return(nil)
         end
 
-        context "#check" do
-          it { is_expected.not_to be_successful_check }
-          it { is_expected.to have_message "This version of ActiveRecord does not support checking whether migrations are pending" }
+        it { is_expected.to be_successful_check }
+        it { is_expected.to have_message "NO pending migrations" }
+      end
+
+      context "with pending migrations" do
+        before do
+          expect(ActiveRecord::Migration)
+            .to receive(rails_pending_migration_method).and_raise(ActiveRecord::PendingMigrationError)
         end
+
+        it { is_expected.not_to be_successful_check }
+        it { is_expected.to have_message "Pending migrations" }
+      end
+
+      context "when active_record.migration_error set to :page_load" do
+        before do
+          allow(Rails.application.config.active_record).to receive(:migration_error).and_return(:page_load)
+        end
+
+        it { is_expected.to be_successful_check }
+        it { is_expected.to have_message "NOTE: pending migrations are checked on page_load" }
       end
     end
   end
